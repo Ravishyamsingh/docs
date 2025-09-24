490aaa31d9956b8a6e96185b1b89503f21218b22
Ravishyam <ravishyam52325@gmail.com>
2025-09-24 18:19:27 +0530

    added first js

---
+++ 02_variable.js
@@ -0,0 +1,22 @@
+console.log("helo")
+console.log(3+3);
+
+
+
+let name="Ravi";
+let num=12;
+let isLoggedIn=false;
+
+//integer =>2 to power 53
+//bigint
+//string=> ""
+//bolean=>true/false
+//null=>empty / standalone;
+//undefine=>variable without any value
+//symbol=> Finding uniqueness
+
+
+
+//object
+console.log(typeof num);
+console.log(typeof undefined);
