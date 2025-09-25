3295d2c18c1473676546672ca2cb96a045bfb4dd
Ravishyam <ravishyam52325@gmail.com>
2025-09-25 07:29:18 +0530

    added third js

--- 01_basic.js
+++ 01_basic.js
@@ -2,3 +2,7 @@ var age=12;
 let num=13;
 const gender=12;
 console.table([age,num,gender]);
+
+
+
+
---
+++ 03_typeConversion.js
@@ -0,0 +1,28 @@
+let score =33;
+let num=null
+let age=undefined
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let ValueInNumber=Number(score);
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+console.log(typeof num);
+console.log(num);
+
+console.log(typeof age);
+console.log(age);
+
+
+let string="5";
+let num2=3;
+console.log(string+num2);
+
+console.log("4"+3);
+
+
+
+
+
